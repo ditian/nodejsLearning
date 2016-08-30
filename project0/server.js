3168ae4ede812d15028712700d09a338ad64c8c8
Di Tian <peacefultd@gmail.com>
2016-08-30 15:01:25 -0700

    upload image and display it in browser

--- project0/server.js
+++ project0/server.js
@@ -5,10 +5,7 @@ function start(route, handle) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " recieved.");
-
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		
-		route(handle, pathname, response);
+		route(handle, pathname, response, request);
 	}
 
 	http.createServer(onRequest).listen(8888);
